fb88aeb5cde7c7275eca58b456469dceefa4882b
Peter Varsanyi <pvarsanyi@patreon.com>
2023-08-04 22:29:54 -0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -624,6 +624,7 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+vim.keymap.set('n', '<leader>vh', require('telescope.builtin').help_tags, {})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
