c7224940cfbdb344a25a5a6d984caaaf2aaf7c56
amtoine <stevan.antoine@gmail.com>
2023-10-07 13:49:47 +0200

    remove spaces in empty lines

--- init.lua
+++ init.lua
@@ -426,10 +426,10 @@ vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
   ensure_installed = { 'java', 'lua', 'python', 'go', 'rust', 'vimdoc', 'vim', 'yaml', 'json', 'json5', 'toml', 'bash'},
-  
+
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
-  
+
     highlight = { enable = true },
     indent = { enable = true },
     incremental_selection = {
