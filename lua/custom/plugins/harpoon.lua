3fb27f5fa84015c0ce5c5ce07c57f77350186afa
Peter Varsanyi <peter.varsanyi@gwelican.eu>
2024-03-27 18:00:57 -0700

    fix

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,7 +1,2 @@
 return {"theprimeagen/harpoon"}
 -- { "theprimeagen/refactoring.nvim"},
--- vim.wo.rnu = true
---   ensure_installed = { 'java', 'lua', 'python', 'go', 'rust', 'vimdoc', 'vim', 'yaml', 'json', 'json5', 'toml', 'bash'},
--- vim.keymap.set('n', '<leader>vh', require('telescope.builtin').help_tags, {})
--- require('mason').setup()
--- require('mason-lspconfig').setup()
