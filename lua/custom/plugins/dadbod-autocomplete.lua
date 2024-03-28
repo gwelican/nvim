e48275c08cc2c0b93821dcc64ba65953a371709f
Peter Varsanyi <peter.varsanyi@gwelican.eu>
2024-03-27 17:55:21 -0700

    add plugins back

---
+++ lua/custom/plugins/dadbod-autocomplete.lua
@@ -0,0 +1,23 @@
+return {
+    "kristijanhusak/vim-dadbod-completion",
+    lazy = true,
+    dependencies = {"tpope/vim-dadbod"},
+    ft = {"sql", "mysql", "plsql"},
+    init = function()
+        vim.api.nvim_create_autocmd("FileType", {
+            group = vim.api.nvim_create_augroup("kide_vim_dadbod_completion", {
+                clear = true
+            }),
+            pattern = {"sql", "mysql", "plsql"},
+            callback = function(event)
+                require("cmp").setup.buffer({
+                    sources = {{
+                        name = "vim-dadbod-completion"
+                    }}
+                })
+            end
+        })
+    end,
+    config = function()
+    end
+}
