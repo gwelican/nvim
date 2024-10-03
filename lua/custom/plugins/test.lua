2957d62ae5fb370e5e98160a12e76e34ef822425
Peter Varsanyi <peter.varsanyi@gwelican.eu>
2024-10-03 12:44:43 -0700

    fix

--- lua/custom/plugins/test.lua
+++ lua/custom/plugins/test.lua
@@ -2,6 +2,6 @@ return {
   'klen/nvim-test',
   config = function()
     require('nvim-test').setup()
-    require('nvim-test.runners.pytest').setup { command = '/home/dev/patreon_py/bin/rdev/pytest' }
+    require('nvim-test.runners.pytest'):setup { command = '/home/dev/patreon_py/bin/rdev/pytest' }
   end,
 }
