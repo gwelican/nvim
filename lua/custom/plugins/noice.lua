437b4907519d72d68fb6755a1725ed66116649dd
Peter Varsanyi <superfly@gwelican.eu>
2025-01-01 22:35:45 -0800

    fix

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -2,9 +2,6 @@
 return {
   'folke/noice.nvim',
   event = 'VeryLazy',
-  opts = {
-    -- add any options here
-  },
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     'MunifTanjim/nui.nvim',
@@ -13,7 +10,15 @@ return {
     --   If not available, we use `mini` as the fallback
     'rcarriga/nvim-notify',
   },
-  
+  opts = {
+    routes = {
+      {
+        view = "notify",
+        filter = { event = "msg_showmode" },
+      },
+    },
+  }
+
   -- config = function()
   --   require('notify').setup {
   --     background_colour = '#000000',
