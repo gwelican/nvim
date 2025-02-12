27df2b820925c12f0c7785f9cdeb8825cc1ad7f8
Peter Varsanyi <peter.varsanyi@gwelican.eu>
2025-02-12 10:29:59 -0800

    fix

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,4 +6,4 @@ vim.opt.swapfile = false
 -- better coop with fzf-lua
 vim.env.FZF_DEFAULT_OPTS = ""
 vim.g.ai_cmp = false
-vim.g.lazyvim_blink_main = not jit.os:find("Windows")
+-- vim.g.lazyvim_blink_main = not jit.os:find("Windows")
