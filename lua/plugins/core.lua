8a68e8922be625eec54a2589628a450cdfddbcdc
Peter Varsanyi <peter.varsanyi@gwelican.eu>
2026-08-25 00:00:53 -0700

    add yamlls

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -5,4 +5,18 @@ return {
       colorscheme = "catppuccin-mocha",
     },
   },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        yamlls = { enabled = false },
+        yayamlls = {
+          mason = false,
+          cmd = { "yayamlls" },
+          filetypes = { "yaml" },
+          root_markers = { ".yayamlls.yaml", ".git" },
+        },
+      },
+    },
+  },
 }
