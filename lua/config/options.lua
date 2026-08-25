7c4ff4abd236719a03d3ad674c2b2d2b9c78d4dc
Peter Varsanyi <peter.varsanyi@gwelican.eu>
2026-08-24 21:51:05 -0700

    cleanup default settings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,9 +3,6 @@
 -- Add any additional options here
 vim.opt.swapfile = false
 
--- better coop with fzf-lua
-vim.env.FZF_DEFAULT_OPTS = ""
-vim.g.ai_cmp = true
 -- vim.g.lazyvim_blink_main = not jit.os:find("Windows")
 vim.opt.clipboard = "unnamedplus"
 if vim.env.SSH_CONNECTION or vim.env.SSH_TTY then
@@ -19,10 +16,6 @@ if vim.env.SSH_CONNECTION or vim.env.SSH_TTY then
     }
   end
 end
-vim.opt.laststatus = 3
 
 -- Enable folding in Neovim
 vim.o.foldmethod = "marker"
-
-vim.o.modeline = true -- Enable modelines
-vim.o.modelines = 5 -- Check first and last 5 lines for modelines
